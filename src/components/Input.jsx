446f5a26b5ac1fd1e10d51cd44bebccd30b8d78d
KWilson718 <wilsonk18@spu.edu>
2024-12-23 18:01:04 -0800

    Added the layout for the input field

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,24 +1,36 @@
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 export default function Input() {
     return (
         <div id='inputComponent'>
             <Box
-                component='form'
+                component="form"
                 sx={{
-                    width: 1/2,
+                    width: 3/4,
                     height: 100,
                     border: 1,
                     padding: 2,
                 }}
+                noValidate
+                autoComplete="off"
             >
-                <TextField
-                    required
-                    id="outlined-required"
-                    label="Required"
-                    defaultValue="Hello World"
-                />
+                <h3 className='inputTitle'>Create Task</h3>
+                <div className='inputRow'>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Task"
+                    />
+
+                    <TextField 
+                        id="outlined"
+                        label="Time Frame"
+                    />
+
+                    <Button variant="contained">Submit Task</Button>
+                </div>
             </Box>
         </div>
     );
